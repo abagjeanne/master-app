5f4acbbe52ed479443639527f88e85d9e9a54c29
2916lalala <74847517+2916lalala@users.noreply.github.com>
2024-04-18 11:25:06 +0800

    wack

--- client/src/companies/GDS.jsx
+++ client/src/companies/GDS.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+  import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import GDSLogo from '../assets/GDS Travel.png';
@@ -7,16 +7,29 @@ const GDS = () => {
   return (
     <div>
       <Header />
-        <div className='row m-5'>
-          <img src ={GDSLogo}></img>
-        <h2 style={{alignItems: 'center'}}>GDS INTERNATIONAL TRAVEL AGENCY INCORPORATED</h2>
+      <div className='container text-center mt-5'>
+        <div className='row'>
+          <div className='col'>
+            <img src={GDSLogo} style={{ width: 400, height: 100 }} alt="GDS Logo" />
+          </div>
         </div>
-
-
-
+        <div className='row'>
+          <div className='col mt-3'>
+            <h4>GDS INTERNATIONAL TRAVEL AGENCY INCORPORATED</h4>
+          </div>
+        </div>
+      </div>
+      <div className='container bg-primary' >
+        <div className='row'>
+          <div className='col'> Select an Application:
+          
+          </div>
+        </div>
+      </div>
       <Footer />
     </div>
   );
 };
+
 
 export default GDS;
