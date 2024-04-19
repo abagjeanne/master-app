fdec318f08ae92531503112b7e61b4159c1f13e0
2916lalala <74847517+2916lalala@users.noreply.github.com>
2024-04-19 09:33:19 +0800

    wack

--- client/src/companies/SUPERNOVA.jsx
+++ client/src/companies/SUPERNOVA.jsx
@@ -1,23 +1,30 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import GDSLogo from '../assets/GDS Travel.png';
+import Supernovalogo from '../assets/Supernova.png';
+import { Link } from 'react-router-dom';
 
 const SUPERNOVA = () =>{
   return(
     <div>
       <Header/>
-        <div className='cons_pflex'>
-          <div className='cons_cflex'> 
-          
+      <div className='cons_pflex m-5'>
+        <div className='cons_cflex' style={{padding: 50}}> 
+          <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            <img src={Supernovalogo} style={{ width: 600, height: 200 }} alt="Supernova Logo" />
           </div>
-          <div className='cons_cflex'>
+            <h3 style={{ textAlign: 'center', marginTop:20 }}>STARLIGHT BUSINESS CONSULTING<br />SERVICES INCORPORATED</h3>
+        </div>
+      <div className='cons_cflex'>
+        <h1>ON PROGRESS...</h1>
+      </div>
+      </div>
+      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <Link to = '/'>
+        <button style={{paddingInline: 60, marginBottom: 20}}>Return To Home</button>
+        </Link>
+      </div>
 
-          </div>
-        </div>
-        <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-        <button >Return To Home</button>
-        </div>
         
       <Footer/>
     </div>
