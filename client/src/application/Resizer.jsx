f5dbf10ef3183c38f7f4c3e711efdada03a2a9a5
abagjeanne <123987707+abagjeanne@users.noreply.github.com>
2024-04-26 18:02:42 +0800

    cropper

--- client/src/application/Resizer.jsx
+++ client/src/application/Resizer.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./appcomp/App.css";
+import { Link } from "react-router-dom";
 import FileInput from "./appcomp/FileInput";
 import ImageCropper from "./appcomp/ImageCropper";
 
@@ -57,7 +57,16 @@ function Resizer() {
   };
 
   return (
-    <div className="container">
+    <div className="m-5">
+        <Link
+            to="#"
+            className="rbtn rbtn-outline"
+            onClick={() => setCurrentPage("choose-img")}
+          >
+            Back
+        </Link>
+    <div className="r-container ">
+
       {currentPage === "choose-img" ? (
         <FileInput onImageSelected={onImageSelected} />
       ) : currentPage === "crop-img" ? (
@@ -68,36 +77,41 @@ function Resizer() {
         />
       ) : (
         <div>
-          <div>
-            <img src={imgAfterCrop} className="cropped-img" alt="Cropped" />
+          {/* <Link
+            to="#"
+            className="rbtn rbtn-outline"
+            onClick={() => setCurrentPage("choose-img")}
+          >
+            Back
+          </Link> */}
+
+            <div className="cropped-img-container">
+              <img
+                src={imgAfterCrop}
+                className="cropped-img"
+                alt="Cropped"
+              />
+            </div>
+            <button onClick={() => setCurrentPage("crop-img")} className="rbtn">
+              Crop
+            </button>
+
+            <button
+              onClick={() => {
+                setCurrentPage("choose-img");
+                setImage("");
+              }}
+              className="rbtn"
+            >
+              New Image
+            </button>
+
+            <button onClick={downloadCroppedImage} className="rbtn">
+              Download Cropped Image
+            </button>
           </div>
-          <button
-            onClick={() => {
-              setCurrentPage("crop-img");
-            }}
-            className="btn"
-          >
-            Crop
-          </button>
-
-          <button
-            onClick={() => {
-              setCurrentPage("choose-img");
-              setImage("");
-            }}
-            className="btn"
-          >
-            New Image
-          </button>
-
-          <button
-            onClick={downloadCroppedImage}
-            className="btn"
-          >
-            Download Cropped Image
-          </button>
-        </div>
       )}
+    </div>
     </div>
   );
 }
--- client/src/application/appcomp/ImageCropper.jsx
+++ client/src/application/appcomp/ImageCropper.jsx
@@ -4,55 +4,62 @@ import Cropper from "react-easy-crop";
 const ImageCropper = ({ image, onCropCancel, onCropDone }) => {
   const [crop, setCrop] = useState({ x: 0, y: 0 });
   const [zoom, setZoom] = useState(1);
-
   const [croppedArea, setCroppedArea] = useState(null);
-  const [aspectRatio, setAspectRatio] = useState(4 / 3);
+  const [aspectRatio, setAspectRatio] = useState({ width: 4, height: 3 });
 
   const onCropComplete = (croppedAreaPercentage, croppedAreaPixels) => {
     setCroppedArea(croppedAreaPixels);
   };
 
   const onAspectRatioChange = (event) => {
-    setAspectRatio(event.target.value);
+    const ratio = JSON.parse(event.target.value);
+    setAspectRatio(ratio);
   };
-
   return (
-    <div className="cropper">
-      <div>
-        <Cropper
-          image={image}
-          aspect={aspectRatio}
-          crop={crop}
-          zoom={zoom}
-          onCropChange={setCrop}
-          onZoomChange={setZoom}
-          onCropComplete={onCropComplete}
-          style={{
-            containerStyle: {
-              width: "100%",
-              height: "80%",
-              background: "#fff",
-            },
-          }}
-        />
-      </div>
-
-      <div className="action-btns">
-        <div className="aspect-ratios" onChange={onAspectRatioChange}>
-          <input type="radio" value={420 / 560} name="ratio" />
-          China Visa (33mm x 48mm)
-          <input type="radio" value={826/ 1062} name="ratio" />
-          Ph Passport (35mm x 45mm)
-          <input type="radio" value={51 / 51} name="ratio" />
-          2X2 and 1x1 ID Size (Box Size)
+    <>
+      <div className="r-container">
+        {/* Cropper */}
+        <div className="cropper">
+          <Cropper
+            image={image}
+            aspect={aspectRatio.width / aspectRatio.height}
+            crop={crop}
+            zoom={zoom}
+            onCropChange={setCrop}
+            onZoomChange={setZoom}
+            onCropComplete={onCropComplete}
+            style={{
+              containerStyle: {
+                width: "100%",
+                background: "#fff",
+              },
+            }}
+          />
         </div>
-
-        <div className="btn-container">
-          <button className="btn btn-outline" onClick={onCropCancel}>
+        <div className="card mx-5 shadow-lg" style={{ backgroundColor: "#EFECEC", width:'500px' }}>
+          {/* Aspect ratio selection */}
+          <div className="action-btns aspect-ratios m-5">
+            <label>
+              <input className="m-2" type="radio" value={JSON.stringify({ width: 420, height: 560 })} name="ratio" onChange={onAspectRatioChange} />
+              China Visa (33mm x 48mm)
+            </label>
+            <label>
+              <input className="m-2" type="radio" value={JSON.stringify({ width: 826, height: 1062 })} name="ratio" onChange={onAspectRatioChange} />
+              Ph Passport (35mm x 45mm)
+            </label>
+            <label >
+              <input className="m-2" type="radio" value={JSON.stringify({ width: 51, height: 51 })} name="ratio" onChange={onAspectRatioChange} />
+              2X2 and 1x1 ID Size (Box Size)
+            </label>
+          </div>
+  
+        {/* Buttons for canceling and applying crop */}
+        <div className="rbtn-container">
+          <button className="rbtn rbtn-outline" onClick={onCropCancel}>
             Cancel
           </button>
           <button
-            className="btn"
+            className="rbtn"
             onClick={() => {
               onCropDone(croppedArea);
             }}
@@ -60,9 +67,11 @@ const ImageCropper = ({ image, onCropCancel, onCropDone }) => {
             Crop & Apply
           </button>
         </div>
+        </div>
       </div>
-    </div>
+    </>
   );
+  
 };
 
 export default ImageCropper;
