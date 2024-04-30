cbf16f88f844992ff874e9629482c00df2ac8940
James De Sena <jamesdesena27@gmail.com>
2024-04-30 15:03:37 +0800

    Revert "Merge branch 'main' of https://github.com/abagjeanne/master-app"
    This reverts commit 232a7eb3cacbc31d31ca0d4bdf4def6522bc5681, reversing
    changes made to dc36e11ada667273c3e3d3e3fabe7a7161037bd5.

--- client/src/components/ContactCard.jsx
+++ client/src/components/ContactCard.jsx
@@ -1,41 +1,46 @@
 import React from 'react';
+// import telegramQR from './path/to/telegramQR.jpg';
+// import wechatQR from './path/to/wechatQR.jpg';
+// import messengerQR from './path/to/messengerQR.jpg';
+// import viberQR from './path/to/viberQR.jpg';
 
-const ContactCard = ({ Name, Position, Number, profile, altText, telegramQR, wechatQR, viberQR }) => {
+const ContactCard = ({ Name, Position, Number, profile, altText, telegramQR, wechatQR, messengerQR, viberQR}) => {
   return (
-    <div className="card p-2 col">
+    <div className="col hover-effect">
       <div className="text-center">
         <img className="rounded-circle" src={profile} width="120px" height="120px" alt={altText} />
       </div>
-      <p style={{ fontWeight: 'bold', fontSize: '20px', marginBottom: '5px', textAlign: 'center' }}>{Name}</p>
-      <p style={{ textAlign: 'center', marginBottom: '10px' }}>{Position}</p>
-      <p style={{ marginBottom: '5px', textAlign: 'center', fontWeight: 'bold' }}>Contact Number/s:</p>
-      <p style={{ textAlign: 'center', marginBottom: '20px' }}>{Number}</p>
-
-      <div className="row justify-content-around">
-        {/* Telegram QR */}
-        {telegramQR &&
-          <div className="col text-center">
-            <p style={{ fontWeight: 'bold', marginBottom: '5px' }}>Telegram</p>
-            <img className='img-hover' src={telegramQR} alt="Telegram QR Code" />
-          </div>
-        }
-
-        {/* WeChat QR */}
-        {wechatQR &&
-          <div className="col text-center">
-            <p style={{ fontWeight: 'bold', marginBottom: '5px' }}>WeChat</p>
-            <img className='img-hover' src={wechatQR} alt="WeChat QR Code" />
-          </div>
-        }
-        {/* Viber QR */}
-        {viberQR &&
-          <div className="col text-center">
-            <p style={{ fontWeight: 'bold', marginBottom: '5px' }}>Viber</p>
-            <img className='img-hover' src={viberQR}  alt="Viber QR Code" />
-          </div>
-        }
+      <p style={{ fontWeight: 'bold', fontSize: '20px', marginBottom: '1px', textAlign: 'center' }}>{Name}</p>
+      <p style={{ textAlign: 'center' }}>{Position}</p>
+      <p style={{ marginBottom: '0px', textAlign: 'center' }}><strong>Contact Number/s:</strong></p>
+      <p style={{ textAlign: 'center' }}>{Number}</p>
+      <div>
+        <div className="table-responsive">
+          <table className="table">
+            <thead>
+              <tr></tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className="text-center" style={{ width: '180.703px', fontWeight: 'bold' }}>Telegram</td>
+                <td className="text-center" style={{ fontWeight: 'bold' }}>WeChat</td>
+              </tr>
+              <tr>
+                <td className="text-center" style={{ width: '180.703px' }}><img src={telegramQR} style={{ width: '150px', height: '150px' }} alt="Telegram QR Code" /></td>
+                <td className="text-center"><img src={wechatQR} style={{ width: '150px', height: '150px' }} alt="WeChat QR Code" /></td>
+              </tr>
+              <tr>
+                <td className="text-center" style={{ width: '180.703px', fontWeight: 'bold' }}>Messenger</td>
+                <td className="text-center" style={{ fontWeight: 'bold' }}>Viber</td>
+              </tr>
+              <tr>
+                <td className="text-center" style={{ width: '180.703px' }}><img src={messengerQR} style={{ width: '150px', height: '150px' }} alt="Messenger QR Code" /></td>
+                <td className="text-center"><img src={viberQR} style={{ width: '150px', height: '150px' }} alt="Viber QR Code" /></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
-
     </div>
   );
 };
