185bc9af9c427b7a280c66a637e9910455ed5e6d
James De Sena <jamesdesena27@gmail.com>
2024-04-30 15:03:51 +0800

    Revert "Reapply "Merge branch 'main' of https://github.com/abagjeanne/master-app""
    This reverts commit 5b7c8a2dd1e46b3ab799c6f55dec705226dca1c2.

--- client/src/components/Link/Links.jsx
+++ client/src/components/Link/Links.jsx
@@ -5,10 +5,8 @@ import { formatDistanceToNow } from 'date-fns';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
-
 const Main = () => {
   const [blogData, setBlog] = useState([]);
-  const [loading, setLoading] = useState(true); // Add loading state
 
   useEffect(() => {
     const fetchLink = async () => {
@@ -22,8 +20,6 @@ const Main = () => {
         }
       } catch (error) {
         console.error(error);
-      } finally {
-        setLoading(false); // Set loading to false after fetching data
       }
     };
     fetchLink();
@@ -36,46 +32,37 @@ const Main = () => {
           <h2 className="display-6" style={{ fontWeight: 'bold' }}>Links</h2>
         </div>
       </div>
-      {loading ? (
-        <div className="loading-container">
-          <div className="loading-spinner"></div>
-          <p className='col text-center'>Loading...</p>
+      {blogData.length === 0 ? (
+        <div className="row">
+          <div className="col text-center no-links-message">
+            <p>No links found</p>
+          </div>
         </div>
       ) : (
-        <div>
-          {blogData.length === 0 ? (
-            <div className="row">
-              <div className="col text-center no-links-message">
-                <p>No links found</p>
-              </div>
-            </div>
-          ) : (
-            <div className="row gy-4">
-              {blogData.map(link => (
-                <div key={link._id} className="col-md-4">
-                  <Link to={`/blog/${link._id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
-                    <div className="card h-100 border-0 shadow hover-effect">
-                      <img src={link.thumbnail.link} className="card-img-top" alt="Blog Post" style={{ objectFit: 'cover', height: '200px' }} />
-                      <div className="m-4" style={{ whiteSpace: 'nowrap' }}>
-                          {link.content ? (
-                            <p dangerouslySetInnerHTML={{ __html: link.content.substring(0, 5) + '...' }} />
-                          ) : (
-                            <p>No content available</p>
-                          )}
-                        </div>
-                      <div className="card-footer bg-transparent border-0">
-                        <div>
-                          <small className="text-muted text-capitalize mx-1 bold">{link.author}</small>
-                          <small className="text-muted mx-1">●</small>
-                          <small className="text-muted">{formatDistanceToNow(new Date(link.dateCreated))} ago</small>
-                        </div>
-                      </div>
+        <div className="row gy-4">
+          {blogData.map(link => (
+            <div key={link._id} className="col-md-4">
+              <Link to={`/blog/${link._id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+                <div className="card h-100 border-0 shadow hover-effect">
+                  <img src={link.thumbnail.link} className="card-img-top" alt="Blog Post" style={{ objectFit: 'cover', height: '200px' }} />
+                  <div className="m-4" style={{ whiteSpace: 'nowrap' }}>
+                      {link.content ? (
+                        <p dangerouslySetInnerHTML={{ __html: link.content.substring(0, 5) + '...' }} />
+                      ) : (
+                        <p>No content available</p>
+                      )}
                     </div>
-                  </Link>
+                  <div className="card-footer bg-transparent border-0">
+                    <div>
+                      <small className="text-muted text-capitalize mx-1 bold">{link.author}</small>
+                      <small className="text-muted mx-1">●</small>
+                      <small className="text-muted">{formatDistanceToNow(new Date(link.dateCreated))} ago</small>
+                    </div>
+                  </div>
                 </div>
-              ))}
+              </Link>
             </div>
-          )}
+          ))}
         </div>
       )}
     </div>
