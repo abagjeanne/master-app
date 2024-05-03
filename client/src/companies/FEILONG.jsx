d813bdc2b78e12e25624bc615dfa1ffa7e08139a
2916lalala <74847517+2916lalala@users.noreply.github.com>
2024-05-03 17:52:41 +0800

    ayyayaya

--- client/src/companies/FEILONG.jsx
+++ client/src/companies/FEILONG.jsx
@@ -10,7 +10,7 @@ const VOLTION = () =>{
           <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
             <img src={Feilong} style={{ width: 400 , height: 'auto' }} alt="Feilong Logo" />
           </div>
-          <h5 style={{ textAlign: 'center', marginTop:30 }}>Feilong Legal</h5>
+          <h5 style={{ textAlign: 'center' }}>FEILONG LEGAL</h5>
         </div>
       <div className='cons_cflex'>
         <h1 style = {{textAlign: 'center'}}>ON PROGRESS...</h1>
@@ -18,9 +18,32 @@ const VOLTION = () =>{
       </div>
       <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
         <Link to = '/'>
-        <button className='rbtn' style={{paddingInline: 60, marginBottom: 50}}>Return To Home</button>
+        <button className='rbtn' style={{paddingInline: 60, marginBottom: 80}}>Return To Home</button>
         </Link>
       </div>
+    {/* UNCOMMENT WHEN THERE ARE NEW APPS 
+      <div className='container text-center mt-4'>
+        <div className='row'>
+          <div className='col'>
+            <img src={Feilong} style={{width: 300, height: 'auto' }} alt="GDS-Intl Logo" />
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col mb-5'>
+            <h5 style={{fontWeight:'bold'}}>FEILONG LEGAL</h5>
+          </div>
+        </div>
+      </div>
+      <div className='container-xxl feilong_innerbox' style = {{marginBottom:35, }}>
+        <h4 style = {{padding:'20px',paddingTop:'50px',textAlign:'center', fontWeight: 'bold', color:'white'}}>Choose an Application to Get Started:</h4>
+        <div className='parent_container'>
+          <Link to = 'http://202.175.224.156:4040' target='_blank'>
+            <div className='feilong_child_containers'>
+              Lorem Ipsum
+            </div>
+          </Link>
+        </div>
+      </div>*/}
     </div>
   );
 };
--- client/src/companies/PDMN.jsx
+++ client/src/companies/PDMN.jsx
@@ -21,6 +21,29 @@ const PDMN = () =>{
         <button className='rbtn' style={{paddingInline: 60, marginBottom: 50}}>Return To Home</button>
         </Link>
       </div>
+      {/* UNCOMMENT WHEN THERE ARE NEW APPS
+      <div className='container text-center mt-4'>
+        <div className='row'>
+          <div className='col'>
+            <img src={PDMNLogo} style={{width: 350, height: 100 }} alt="GDS-Intl Logo" />
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col mt-3 mb-5'>
+            <h5 style={{fontWeight:'bold'}}>PHILLIPINE DRAGON MEDIA NETWORK CORPORATION</h5>
+          </div>
+        </div>
+      </div>
+      <div className='container-xxl pdmn_innerbox' style = {{marginBottom:35, }}>
+        <h4 style = {{padding:'20px',paddingTop:'50px',textAlign:'center', fontWeight: 'bold', color:'white'}}>Choose an Application to Get Started:</h4>
+        <div className='parent_container'>
+          <Link to = 'http://202.175.224.156:4040' target='_blank'>
+            <div className='pdmn_child_containers'>
+              Lorem Ipsum
+            </div>
+          </Link>
+        </div>
+      </div> */}
     </div>
   );
 };
